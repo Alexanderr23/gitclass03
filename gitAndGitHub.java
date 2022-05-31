ebcc5d8136595788f14fe51f5d4b0e5970ed4e2c
Alexanderr23 <ar27109@gmail.com>
2022-05-31 19:54:57 -0400

    2nd commit

--- gitAndGitHub.java
+++ gitAndGitHub.java
@@ -4,7 +4,16 @@ public class gitAndGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("today we learn github");
+	System.out.println("today is a good day");
+	//1.navigate to terminal to the location we want
+	//2.intialize our rep
+	
+	
+	
+	
+	
+	
 	}
 
 }
